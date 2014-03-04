2111d8ce11bb0bc00f7cee06f30f726c83962ade
Nicholas Wong <nickwph@gmail.com>
2014-03-03 19:21:36 -0500

    Added view for bill calculator in storyboard. Added view controller class for it. All functions are working so far.

---
+++ BillCalculatorPrototype/BillCalculatorViewController.h
@@ -0,0 +1,12 @@
+//
+//  BillCalculatorViewController.h
+//  BillCalculatorPrototype
+//
+//  Created by Nicholas Wong on 2/28/14.
+//  Copyright (c) 2014 Nicholas Workshop. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BillCalculatorViewController : UITableViewController
+@end
---
+++ BillCalculatorPrototype/BillCalculatorViewController.m
@@ -0,0 +1,85 @@
+//
+//  BillCalculatorViewController.m
+//  BillCalculatorPrototype
+//
+//  Created by Nicholas Wong on 2/28/14.
+//  Copyright (c) 2014 Nicholas Workshop. All rights reserved.
+//
+
+#import "BillCalculatorViewController.h"
+#import "Bill.h"
+
+@interface BillCalculatorViewController ()
+@property int splitCount;
+@property Bill *bill;
+@property(weak, nonatomic) IBOutlet UITextField *uCheckAmount;
+@property(weak, nonatomic) IBOutlet UISwitch *uTaxIncluded;
+@property(weak, nonatomic) IBOutlet UILabel *uTotalCharge;
+@property(weak, nonatomic) IBOutlet UILabel *uTotalTips;
+@property(weak, nonatomic) IBOutlet UILabel *uTipRate;
+@property(weak, nonatomic) IBOutlet UILabel *uSplitCount;
+@property(weak, nonatomic) IBOutlet UILabel *uTotalChargePerPerson;
+@property(weak, nonatomic) IBOutlet UILabel *uTotalTipPerPerson;
+@end
+
+@implementation BillCalculatorViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+
+    // set init data
+    _splitCount = 1;
+    _bill = [[Bill alloc] init];
+    [_uCheckAmount setText:[NSString stringWithFormat:@"$%g", _bill.checkAmount]];
+    [_uTipRate setText:[NSString stringWithFormat:@"(%g%%)", _bill.tipRate * 100]];
+    [_uTaxIncluded setOn:_bill.isTaxIncluded];
+    [self updateResult];
+
+    // set delegates
+    [self.view addGestureRecognizer:[[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(dismissKeyboard)]];
+}
+
+- (void)updateResult
+{
+    [_bill calculate];
+    [_uTotalCharge setText:[NSString stringWithFormat:@"$%g", _bill.totalCharge]];
+    [_uTotalTips setText:[NSString stringWithFormat:@"$%g", _bill.totalTip]];
+    [_uTotalChargePerPerson setText:[NSString stringWithFormat:@"$%g", _bill.totalCharge / _splitCount]];
+    [_uTotalTipPerPerson setText:[NSString stringWithFormat:@"$%g", _bill.totalTip / _splitCount]];
+}
+
+- (IBAction)checkAmountEditingDidEnd:(UITextField *)sender
+{
+    [_bill setCheckAmount:[sender.text floatValue]];
+    [self updateResult];
+}
+
+- (IBAction)taxIncludedValueChanged:(UISwitch *)sender
+{
+    [_bill setIsTaxIncluded:sender.isOn];
+    [self updateResult];
+}
+
+- (IBAction)tipRateValueChanged:(UISlider *)sender
+{
+    float tipRate = sender.value * 5 / 100;
+    int tipRatePercent = (int) sender.value * 5;
+    [_uTipRate setText:[NSString stringWithFormat:@"(%d%%)", tipRatePercent]];
+    [_bill setTipRate:tipRate];
+    [self updateResult];
+}
+
+- (IBAction)splittingValueChanged:(UISlider *)sender
+{
+    _splitCount = (int) sender.value;
+    [_uSplitCount setText:[NSString stringWithFormat:@"(%d)", _splitCount]];
+    [self updateResult];
+}
+
+- (void)dismissKeyboard
+{
+    [_uCheckAmount resignFirstResponder];
+}
+
+@end
